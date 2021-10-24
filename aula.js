bd9af46b1129c6d3d5340d4e51e198f71df59c67
Rute <rute.leone@hotmail.com>
2021-10-24 11:04:40 -0300

    Primeiro commit

---
+++ aula.js
@@ -0,0 +1,15 @@
+console.log("Oi meu nome é Rute");
+console.log("Bem Vindo ao teste");
+
+//declarando variável(camelCase) e imprimindo com Template String 
+
+let idadeRute = 23;
+console.log(`Eu tenho ${idadeRute} anos`); 
+
+//Atividade: Imprimir três variáveis na tela 
+
+let faculdade = "Estácio";
+let modalidade = "ead";
+let periodo = "3°";
+
+console.log(`Eu estudo na ${faculdade}, modalidade ${modalidade} e estou no ${periodo} período`);
